8ff08d88e291f3761baa8fb40901f5595d527954
ad-2703 <56711397+ad-2703@users.noreply.github.com>
2021-10-05 14:07:17 -0400

    Update aqi-pred.py

--- aqi-pred.py
+++ aqi-pred.py
@@ -2,6 +2,8 @@ import csv
 import math
 import random
 import numpy as np
+from sklearn.linear_model import LinearRegression
+
 def calc_err(w1, w2,  b, pm, dates, y_all):
     total = 0
     for i in range (len(pm)):
@@ -12,6 +14,8 @@ def calc_err(w1, w2,  b, pm, dates, y_all):
 
 def calc_err_one(w1, w2, b, pm_10, aqi, dates):
     y_hat = (w1*pm_10)+ (w2*dates) + b
+    print(y_hat)
+    print(aqi)
     return (aqi-y_hat)**2
 
 
@@ -19,7 +23,7 @@ learning_rate = 0.6
 
 def get_date(x):
     x = x.split("/")
-    return float(x[0])+ (10*float(x[1])) + (10*float(x[2]))
+    return float(x[0])+ (10*float(x[1])) + (float(x[2]))
 
 
 def minimize_err(w1, w2, b, pm_10, aqi, dates):
@@ -27,10 +31,11 @@ def minimize_err(w1, w2, b, pm_10, aqi, dates):
 
     i = random.randint(0, len(aqi)-1)
     err = calc_err_one(w1, w2, b, pm_10[i], aqi[i], dates[i])
-    w1_new =w1- 2*err*pm_10[i]
 
-    w2_new =w2- 2*err*dates[i]
+    w1_new =w1- 2*err*pm_10[i]*learning_rate
 
+    w2_new =0
+    print(w1_new, w2_new)
     b_new = b-2*err*1
     return w1_new, w2_new, b_new
 
@@ -47,22 +52,31 @@ if __name__=="__main__":
         data.append(elem.split(","))
     print(data[0])
 
-    w1 = 0
-    w2=0
-    b = 0
+     
+    pm_10 = [[float(data_line[4][1:-1]) , float(data_line[-1][1:-3])]for data_line in data]
 
-    pm_10 = [float(data_line[4][1:-1]) for data_line in data]
     aqi = [float(data_line[6][1:-1]) for data_line in data]
-    dates = [get_date(data_line[0][1:-1]) for data_line in data]
 
-    while(True):
-        w1_bar, w2_bar , b = minimize_err(w1, w2,b, pm_10, aqi, dates)
-        
-        if (w1_bar - w1)**2 + (w2_bar-w2)**2 < math.exp(-3):
-            break
-        w1, w2=w1_bar, w2_bar
 
-    print(w1, w2)
+    #lati = [float(data_line[-1][1:-1]) for data_line in data]
+
+
+
+    reg = LinearRegression().fit(pm_10[0:len(pm_10)//2], aqi[0:len(pm_10)//2])
+    print(reg.coef_)
+
+    #calc error
+
+    tot = 0
+
+    for i in range(len(pm_10)//2, len(pm_10)):
+        y = reg.predict(np.array([pm_10[i]]))
+        err = (y - aqi[i])**2
+        tot += err
+
+    print("err")
+    print(tot/len(aqi))
+
 
 
 
